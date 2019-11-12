ec74842b31adb905f7c4d2c2aa3e89885c672843
Yuriy Karpov <ukarpov@gmail.com>
2019-11-12 20:26:37 +0300

    1. Конструктор по умолчанию.[#192417] (занятие)

---
+++ chapter_002/src/main/java/tracker/Item.java
@@ -0,0 +1,13 @@
+package tracker;
+
+public class Item {
+
+    public Item() {
+        super();
+        System.out.println("load item");
+    }
+
+    public static void main(String[] args) {
+        Item item = new Item();
+    }
+}
