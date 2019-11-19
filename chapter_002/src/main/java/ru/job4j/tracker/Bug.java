b704bf280f0890d30e091c297b36273c877ae0f0
ukarpov <ukarpov@gmail.com>
2019-11-19 17:38:32 +0300

    тесты

--- chapter_002/src/main/java/ru/job4j/tracker/Bug.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Bug.java
@@ -1,5 +1,22 @@
 package ru.job4j.tracker;
 
-public class Bug extends Item {
+import java.util.Arrays;
 
+public class Bug extends Item {
+    public Bug() {
+        super("Some bug");
+    }
+
+    public static void main(String[] args) {
+        int[] a1 = {0,1,2,3,4,5,6,7,8,9};
+        int[] a2 = {-1};
+
+        int idx = 3;
+        int position = 9;
+        System.out.println(Arrays.toString(a1));
+        System.arraycopy(a1, idx + 1, a1, idx, a1.length - idx - 1);
+        System.out.println(Arrays.toString(a1));
+        System.arraycopy(a2, 0, a1, position, a1.length - position);
+        System.out.println(Arrays.toString(a1));
+    }
 }
