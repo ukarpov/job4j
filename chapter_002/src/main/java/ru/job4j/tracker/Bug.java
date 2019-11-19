f29d4ff6f9b2e64c36635beab5c10d4f18642627
ukarpov <ukarpov@gmail.com>
2019-11-19 18:05:39 +0300

    Revert "тесты"
    This reverts commit 5435420b

--- chapter_002/src/main/java/ru/job4j/tracker/Bug.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Bug.java
@@ -7,16 +7,16 @@ public class Bug extends Item {
         super("Some bug");
     }
 
-//    public static void main(String[] args) {
-//        int[] a1 = {0,1,2,3,4,5,6,7,8,9};
-//        int[] a2 = {-1};
-//
-//        int idx = 3;
-//        int position = 9;
-//        System.out.println(Arrays.toString(a1));
-//        System.arraycopy(a1, idx + 1, a1, idx, a1.length - idx - 1);
-//        System.out.println(Arrays.toString(a1));
-//        System.arraycopy(a2, 0, a1, position, a1.length - position);
-//        System.out.println(Arrays.toString(a1));
-//    }
+    public static void main(String[] args) {
+        int[] a1 = {0,1,2,3,4,5,6,7,8,9};
+        int[] a2 = {-1};
+
+        int idx = 3;
+        int position = 9;
+        System.out.println(Arrays.toString(a1));
+        System.arraycopy(a1, idx + 1, a1, idx, a1.length - idx - 1);
+        System.out.println(Arrays.toString(a1));
+        System.arraycopy(a2, 0, a1, position, a1.length - position);
+        System.out.println(Arrays.toString(a1));
+    }
 }
