3705848e3b693af076b5b4b32f43d2fc7e5ac553
ukarpov <ukarpov@gmail.com>
2019-11-19 11:12:58 +0300

    исправлена структура пакетов

--- chapter_002/src/main/java/ru/job4j/tracker/Predator.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Predator.java
@@ -1,4 +1,4 @@
-package tracker;
+package ru.job4j.tracker;
 
 public class Predator extends Animal {
     public Predator(String name) {
--- chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tiger.java
@@ -1,4 +1,4 @@
-package tracker;
+package ru.job4j.tracker;
 
 public class Tiger extends Predator {
     public Tiger(String name) {
