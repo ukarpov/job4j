bef35b523d328373170544262609183bd1cc7429
Yuriy Karpov <ukarpov@gmail.com>
2019-11-21 20:39:10 +0300

    небольшая правка тестов

--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -18,15 +18,10 @@ public class TrackerTest {
     public void whenReplaceNameThenReturnNewName() {
         Tracker tracker = new Tracker();
         Item previous = new Item("test1");
-        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
         tracker.add(previous);
-        // Создаем новую заявку.
         Item next = new Item("test2");
-        // Проставляем старый id из previous, который был сгенерирован выше.
         next.setId(previous.getId());
-        // Обновляем заявку в трекере.
         tracker.replace(previous.getId(), next);
-        // Проверяем, что заявка с таким id имеет новые имя test2.
         assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
     }
 
@@ -34,11 +29,8 @@ public class TrackerTest {
     public void whenDeleteThenReturnNull() {
         Tracker tracker = new Tracker();
         Item i1 = new Item("test1");
-        // Добавляем заявку в трекер. Теперь в объект проинициализирован id.
         tracker.add(i1);
-        // Удаляем заявку
         tracker.delete(i1.getId());
-        // Проверяем, что заявки с таким id нет
         assertNull(tracker.findById(i1.getId()));
     }
 
@@ -78,6 +70,6 @@ public class TrackerTest {
         Item i3 = new Item("test1");
         tracker.add(i3);
         Item[] allItems = tracker.findByName("test5");
-        assertNull(allItems);
+        assertArrayEquals(new Item[]{}, allItems);
     }
 }
