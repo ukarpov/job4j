a04acc8214d937f842cab95b908b70f5dbb89386
Yuriy Karpov <ukarpov@gmail.com>
2019-11-26 21:19:19 +0300

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#192391]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -20,7 +20,7 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Input input = new ValidateInput();
+        Input input = new ValidateInput(new ConsoleInput());
         Tracker tracker = new Tracker();
         UserAction[] actions = {
                 new CreateAction(),
