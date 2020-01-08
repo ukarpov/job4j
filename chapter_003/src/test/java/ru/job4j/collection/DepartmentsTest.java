a7d38a42400643da4f3329b3a19c3e6f5cc9d8a0
Yuriy Karpov <ukarpov@gmail.com>
2020-01-08 19:04:59 +0300

    1. Collection API Улучшения[#192235]

--- chapter_003/src/test/java/ru/job4j/collection/DepartmentsTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/DepartmentsTest.java
@@ -45,20 +45,20 @@ public class DepartmentsTest {
     }
     @Test
     public void whenDescSort() {
-        List<String> input = Arrays.asList("K1/SK1/SSK1",
-                                           "K2/SK1/SSK1",
-                                           "K1/SK2",
-                                           "K2/SK1/SSK2",
-                                           "K1/SK1/SSK2");
-        List<String> expect = Arrays.asList("K2",
-                                            "K2/SK1",
-                                            "K2/SK1/SSK2",
-                                            "K2/SK1/SSK1",
-                                            "K1",
-                                            "K1/SK2",
-                                            "K1/SK1",
-                                            "K1/SK1/SSK2",
-                                            "K1/SK1/SSK1");
+        List<String> input = List.of("K1/SK1/SSK1",
+                                     "K2/SK1/SSK1",
+                                     "K1/SK2",
+                                     "K2/SK1/SSK2",
+                                     "K1/SK1/SSK2");
+        List<String> expect = List.of("K2",
+                                      "K2/SK1",
+                                      "K2/SK1/SSK2",
+                                      "K2/SK1/SSK1",
+                                      "K1",
+                                      "K1/SK2",
+                                      "K1/SK1",
+                                      "K1/SK1/SSK2",
+                                      "K1/SK1/SSK1");
         List<String> result = Departments.fillGaps(input);
         Departments.sortDesc(result);
         assertThat(result, is(expect));
