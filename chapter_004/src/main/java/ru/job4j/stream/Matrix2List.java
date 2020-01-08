4884b3bde2e978ced17897a25a03e3ae44f200d7
Yuriy Karpov <ukarpov@gmail.com>
2020-01-08 15:58:41 +0300

    исправление стиля

--- chapter_004/src/main/java/ru/job4j/stream/Matrix2List.java
+++ chapter_004/src/main/java/ru/job4j/stream/Matrix2List.java
@@ -6,8 +6,8 @@ import java.util.stream.Stream;
 public class Matrix2List {
     public static void main(String[] args) {
         Integer[][] iArr = new Integer[3][3];
-        for(int i = 0; i < iArr.length; i++) {
-            for(int j = 0; j < iArr[i].length; j++) {
+        for (int i = 0; i < iArr.length; i++) {
+            for (int j = 0; j < iArr[i].length; j++) {
                 iArr[i][j] = i * iArr[i].length + j + 1;
             }
         }
