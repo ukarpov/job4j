b9ef07a496ea1bbee2ba7e5a09b431d22d5761fb
Yuriy Karpov <ukarpov@gmail.com>
2020-01-23 21:29:58 +0300

    5.1.2. Создать итератор четные числа[#223732] оставлен один индекс

--- chapter_005/src/main/java/ru/job4j/collections/EvenNumbersIterator.java
+++ chapter_005/src/main/java/ru/job4j/collections/EvenNumbersIterator.java
@@ -6,14 +6,13 @@ import java.util.NoSuchElementException;
 public class EvenNumbersIterator implements Iterator {
     private final int[] nums;
     private int lastIndex = 0;
-    private int lastEvenIndex = 0;
 
     public EvenNumbersIterator(final int[] numbers) {
         this.nums = numbers;
     }
 
     private int getNextEvenIndex(int index) {
-        int result = -1;
+        int result = nums.length;
         for (int i = index; i < nums.length; i++) {
             if (nums[i] % 2 == 0) {
                 result = i;
@@ -25,8 +24,8 @@ public class EvenNumbersIterator implements Iterator {
 
     @Override
     public boolean hasNext() {
-        lastEvenIndex = getNextEvenIndex(this.lastIndex);
-        return lastEvenIndex >= 0;
+        lastIndex = getNextEvenIndex(this.lastIndex);
+        return lastIndex < nums.length;
     }
 
     @Override
@@ -34,7 +33,6 @@ public class EvenNumbersIterator implements Iterator {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        this.lastIndex = lastEvenIndex;
         return nums[this.lastIndex++];
     }
 }
