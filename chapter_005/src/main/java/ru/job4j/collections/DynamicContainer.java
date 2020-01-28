89df24408ff275114fb000c6dda2f053e6c8c53f
ukarpov <ukarpov@gmail.com>
2020-01-28 10:56:21 +0300

    5.3.1. Создать динамический список на базе массива.[#223726]

---
+++ chapter_005/src/main/java/ru/job4j/collections/DynamicContainer.java
@@ -0,0 +1,63 @@
+package ru.job4j.collections;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class DynamicContainer<E> implements Iterable<E> {
+    private final int INITIAL_SIZE = 10;
+
+    private E[] array;
+    private int arrayIndex = 0;
+    private int modCount = 0;
+
+    public DynamicContainer() {
+        initArray(INITIAL_SIZE);
+    }
+
+    public DynamicContainer(int size) {
+        initArray(size);
+    }
+
+    public E get(int index) {
+        return array[index];
+    }
+
+    public void add(E value) {
+        if (arrayIndex >= array.length) {
+            E[] temp = array;
+            initArray(array.length + 1);
+            System.arraycopy(temp, 0, array, 0, temp.length);
+        }
+        array[arrayIndex++] = value;
+        modCount++;
+    }
+
+    private void initArray(int size) {
+        array =  (E[]) new Object[size];
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int currentIndex = 0;
+            private final int expectedModCount = modCount;
+
+            @Override
+            public boolean hasNext() {
+                if (expectedModCount != modCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return currentIndex < array.length;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return array[currentIndex++];
+            }
+        };
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/collections/DynamicContainerTest.java
@@ -0,0 +1,56 @@
+package ru.job4j.collections;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class DynamicContainerTest {
+
+    private DynamicContainer<String> ds;
+
+    @Before
+    public void initTestData() {
+        ds = new DynamicContainer<>(2);
+        ds.add("A");
+        ds.add("B");
+    }
+
+    @Test
+    public void whenGetElement() {
+        assertTrue(ds.get(0).equals("A"));
+    }
+
+    @Test
+    public void whenAddAnotherElementThenResize() {
+        ds.add("C");
+        assertTrue(ds.get(2).equals("C"));
+    }
+
+    @Test
+    public void testIterator() {
+        Iterator<String> it = ds.iterator();
+        assertThat(it.hasNext(), is(true));
+        assertTrue(it.next().equals("A"));
+    }
+
+    @Test
+    public void testIteratorFailFast() {
+        Iterator<String> it = ds.iterator();
+        ds.add("D");
+        boolean wasError = false;
+        try {
+            it.hasNext();
+        } catch(ConcurrentModificationException cme) {
+            wasError = true;
+        }
+        assertTrue(wasError);
+    }
+
+
+}
