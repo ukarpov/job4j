f1c37d82b56915ef0bbe59f5076b18c872dba360
Yuriy Karpov <ukarpov@gmail.com>
2020-02-06 21:15:25 +0300

    4. Переопределить только equals[#223747]

--- chapter_005/src/main/java/ru/job4j/collections/User.java
+++ chapter_005/src/main/java/ru/job4j/collections/User.java
@@ -14,18 +14,18 @@ public class User {
         this.birthday = birthday;
     }
 
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        User user = (User) o;
-//        return children == user.children &&
-//                name.equals(user.name) &&
-//                birthday.equals(user.birthday);
-//    }
-
     @Override
-    public int hashCode() {
-        return Objects.hash(name, children, birthday);
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children &&
+                name.equals(user.name) &&
+                birthday.equals(user.birthday);
     }
+
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(name, children, birthday);
+//    }
 }
