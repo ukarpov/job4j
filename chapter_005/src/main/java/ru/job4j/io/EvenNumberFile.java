d696999371bf83fab8d19f274ab92252989787f5
ukarpov <ukarpov@gmail.com>
2020-04-07 10:37:37 +0300

    0. Видео[#223766]

---
+++ chapter_005/src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder();
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        String[] lines = text.toString().split(System.lineSeparator());
+        for (String line : lines) {
+            if (Integer.valueOf(line) % 2 == 0) {
+                System.out.println(line);
+            }
+        }
+    }
+}
