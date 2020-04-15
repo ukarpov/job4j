e424f27adba4ea36b8d318822accd55de41d0a29
Yuriy Karpov <ukarpov@gmail.com>
2020-04-15 12:52:28 +0300

    2. Тестирование IO[#223761]

--- chapter_006/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_006/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,21 +1,33 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 
 import static org.junit.Assert.assertTrue;
 
 public class AnalizyTest {
-    @Test
-    public void whenStartsWorking() {
-        String srcPath = "./data/server.log";
-        String tgtPath = "./data/unavailable.csv";
-        Analizy an = new Analizy();
-        an.unavailable(srcPath, tgtPath);
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
-        try (BufferedReader read = new BufferedReader(new FileReader(tgtPath))) {
+    @Test
+    public void whenStartsWorking() throws IOException {
+        File source = folder.newFile("server.log");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01" + System.lineSeparator() +
+                        "500 10:57:01" + System.lineSeparator() +
+                        "400 10:58:01" + System.lineSeparator() +
+                        "200 10:59:01" + System.lineSeparator() +
+                        "500 11:01:02" + System.lineSeparator() +
+                        "200 11:02:02");
+        }
+        File target = folder.newFile("unavailable.csv");
+        Analizy an = new Analizy();
+        an.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+
+        try (BufferedReader read = new BufferedReader(new FileReader(target.getAbsolutePath()))) {
             String line = read.readLine();
             assertTrue(line.equals("10:57:01;10:59:01"));
         } catch (Exception e) {
@@ -24,13 +36,16 @@ public class AnalizyTest {
     }
 
     @Test
-    public void whenOpenPeriod() {
-        String srcPath = "./data/server2.log";
-        String tgtPath = "./data/unavailable2.csv";
+    public void whenOpenPeriod() throws IOException {
+        File source = folder.newFile("server.log");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("500 10:57:01");
+        }
+        File target = folder.newFile("unavailable.csv");
         Analizy an = new Analizy();
-        an.unavailable(srcPath, tgtPath);
+        an.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
 
-        try (BufferedReader read = new BufferedReader(new FileReader(tgtPath))) {
+        try (BufferedReader read = new BufferedReader(new FileReader(target.getAbsoluteFile()))) {
             String line = read.readLine();
             assertTrue(line.equals("10:57:01;"));
         } catch (Exception e) {
