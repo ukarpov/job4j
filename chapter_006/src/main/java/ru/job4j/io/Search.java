4a8d803122dd281fc8c0e316b458fc8349bc1057
Yuriy Karpov <ukarpov@gmail.com>
2020-05-05 09:56:38 +0300

    3. Сканирование файловой системы.[#223760] / исправления

--- chapter_006/src/main/java/ru/job4j/io/Search.java
+++ chapter_006/src/main/java/ru/job4j/io/Search.java
@@ -5,6 +5,7 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import static java.nio.file.FileVisitResult.CONTINUE;
 
@@ -14,33 +15,44 @@ public class Search {
         Files.walkFileTree(start, new PrintFiles());
     }
 
+    static class FileVisitorWithPredicate implements FileVisitor<Path> {
+
+        private Predicate<Path> p;
+        private List<String> l;
+
+        FileVisitorWithPredicate(Predicate<Path> p, List<String> l) {
+            this.p = p;
+            this.l = l;
+        }
+
+        @Override
+        public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            if (p.test(file)) {
+                l.add(file.getFileName().toString());
+            }
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+            return CONTINUE;
+        }
+
+        @Override
+        public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+            return CONTINUE;
+        }
+    }
+
     public static List<String> search(Path root, String ext) {
         List<String> result = new ArrayList<>();
         try {
-            Files.walkFileTree(root, new FileVisitor<Path>() {
-                @Override
-                public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                    return CONTINUE;
-                }
-
-                @Override
-                public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                    if (file.getFileName().toString().endsWith(ext)) {
-                        result.add(file.getFileName().toString());
-                    }
-                    return CONTINUE;
-                }
-
-                @Override
-                public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
-                    return CONTINUE;
-                }
-
-                @Override
-                public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
-                    return CONTINUE;
-                }
-            });
+            Files.walkFileTree(root, new FileVisitorWithPredicate(path -> path.getFileName().toString().endsWith(ext), result));
         } catch (Exception e) {
             e.printStackTrace();
         }
