1842e671434655368b74703de404f379b86a5aff
ukarpov <ukarpov@gmail.com>
2020-08-23 14:32:01 +0300

    1. Quartz

--- chapter_007/src/main/java/quartz/AlertRabbit.java
+++ chapter_007/src/main/java/quartz/AlertRabbit.java
@@ -2,18 +2,29 @@ package quartz;
 
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
 public class AlertRabbit {
     public static void main(String[] args) {
+        Properties cfg = new Properties();
+        try (FileInputStream in = new FileInputStream("chapter_007\\src\\main\\resources\\rabbit.properties")) {
+            cfg.load(in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         try {
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDetail job = newJob(Rabbit.class).build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(10)
+                    .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
