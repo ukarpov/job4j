4787f0fe0dda4ae85ca986e13501190a88833289
Yuriy Karpov <ukarpov@gmail.com>
2020-09-17 21:07:08 +0300

    1. Исправить код класс SimpleGenerator / исправления

--- chapter_008/src/test/java/GeneratorTest.java
+++ chapter_008/src/test/java/GeneratorTest.java
@@ -16,50 +16,32 @@ public class GeneratorTest {
         assertTrue("str1 = aaa, str2 = bbb".equals(gen.produce(template, substs)));
     }
 
-    @Test
+    @Test(expected=Exception.class)
     public void whenNoValuesInMap() {
         String template = "str1 = ${str1}, str2 = ${str2}";
         Map<String, String> substs = new HashMap<>();
-        boolean wasEx = false;
-        try {
-            Generator gen = new GeneratorStr();
-            gen.produce(template, substs);
-        } catch(Exception e) {
-            wasEx = true;
-        }
-        assertTrue(wasEx);
+        Generator gen = new GeneratorStr();
+        gen.produce(template, substs);
     }
 
-    @Test
+    @Test(expected=Exception.class)
     public void whenLessValuesInMap() {
         String template = "str1 = ${str1}, str2 = ${str2}";
         Map<String, String> substs = new HashMap<>();
         substs.put("str1", "aaa");
-        boolean wasEx = false;
-        try {
-            Generator gen = new GeneratorStr();
-            gen.produce(template, substs);
-        } catch(Exception e) {
-            wasEx = true;
-        }
-        assertTrue(wasEx);
+        Generator gen = new GeneratorStr();
+        gen.produce(template, substs);
     }
 
-    @Test
+    @Test(expected=Exception.class)
     public void whenTooMuchValuesInMap() {
         String template = "str1 = ${str1}, str2 = ${str2}";
         Map<String, String> substs = new HashMap<>();
         substs.put("str1", "aaa");
         substs.put("str2", "bbb");
         substs.put("str3", "bbb");
-        boolean wasEx = false;
-        try {
-            Generator gen = new GeneratorStr();
-            gen.produce(template, substs);
-        } catch(Exception e) {
-            wasEx = true;
-        }
-        assertTrue(wasEx);
+        Generator gen = new GeneratorStr();
+        gen.produce(template, substs);
     }
 
     @Test
